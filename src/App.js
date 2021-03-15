0feb1e1c361242daae29d1a1536b4e6176783e39
vermad843 <vishnuverma7524@gmail.com>
2021-03-16 01:24:36 +0530

    header main footer

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="grid-container">
+        <header>
+             <a href = "/">React Shopping Cart</a>
+        </header>
+        <main>
+           Product List
+        </main>
+        <footer>
+           All right reserved@2021
+        </footer>
+    </div>
+  );
+}
+
+export default App;
