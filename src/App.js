90756c51f473b17a4bce118df1a3c4c0f0975b53
vermad843 <vishnuverma7524@gmail.com>
2021-03-16 05:21:38 +0530

    sortProducts

--- src/App.js
+++ src/App.js
@@ -13,9 +13,27 @@ class App extends Component {
           };
      }
 
-    sortProducts(sort) {
-
-    }
+    sortProducts = (e) => {
+         const sort = e.target.value;
+         this.setState((state) => ({
+              sort : sort,
+              products : this.state.products
+                  .slice()
+                   .sort((a, b) => 
+                    sort === "lowest" 
+                      ? a.price > b.price
+                        ? 1
+                        :-1
+                      : sort === "highest"
+                      ? a.price < b.price
+                        ? 1
+                        :-1
+                      : a._id > b._id
+                        ? 1
+                        :-1    
+              ),
+         }));
+    };
 
     filterProducts = (e) => {
        if(e.target.value === "") {
