0ff7a2d84c201638e1aff0f4405716c1c66fe55a
vermad843 <vishnuverma7524@gmail.com>
2021-03-20 09:51:02 +0530

    actions

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 [x] npm  i react-reveal
 
 [x] Modal - npm i react-modal
+
+[x] npm i redux react-redux redux-thunk
---
+++ src/actions/productAction.js
@@ -0,0 +1,10 @@
+import { FETCH_PRODUCTS } from '../types';
+ 
+
+export const fetchProducts = () => async(dispatch) => {
+    const res  = await fetch("/api/products");
+    dispatch({
+        type : FETCH_PRODUCTS,
+        payload : res.data,
+    });
+};
